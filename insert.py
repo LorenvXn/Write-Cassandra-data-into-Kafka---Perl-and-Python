637c1625471e6e9c1c4a72977558f73f33d24c90
Satanette <predark.dante@gmail.com>
2017-03-01 15:32:05 +0200

    Update insert.py

--- insert.py
+++ insert.py
@@ -10,7 +10,18 @@ rows=connection.execute("insert into users(id, email, name, password) values('us
 
 
 
-#######################################
+############################################################
 # n.b: 'rapcas' keyspace is already 
 #  created
-#######################################
+#
+#////////////////////////////////////////
+#  Output:
+#  
+#cqlsh:rapcas> select * from rapcas.users;
+#
+# id    | email             | name | password
+#-------+-------------------+------+--------------
+#user2 | user2@example.com |  foo | super secret
+# user1 | user1@example.com |  foo | super secret
+#user3 | user3@example.com |  foo | super secret
+###########################################################
